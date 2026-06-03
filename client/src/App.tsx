16938b2c5203a3a437835f3b5d91752a081b5652
eunnn <ehddms0517@naver.com>
2026-06-04 00:22:41 +0900

    feat: 라우터 설정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,18 @@
 import { Global } from '@emotion/react';
 import { globalStyles } from './styles/global';
+import { Routes, Route } from 'react-router-dom';
+
+import CartPage from './pages/cart/CartPage';
+import CheckoutPage from './pages/checkout/CheckoutPage';
 
 function App() {
   return (
     <>
       <Global styles={globalStyles} />
+      <Routes>
+        <Route path="/" element={<CartPage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
+      </Routes>
     </>
   );
 }

d4acbd0ce345bd3808a03d368c50c1a0ea688e72
eunnn <ehddms0517@naver.com>
2026-06-04 20:37:26 +0900

    chore: RTL 테스트 환경 설정

--- client/jest.config.cjs
+++ client/jest.config.cjs
@@ -1,6 +1,28 @@
 module.exports = {
-  testEnvironment: 'node',
+  testEnvironment: 'jest-fixed-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   transform: {
-    '^.+\\.(ts|tsx)$': ['@swc/jest'],
+    '^.+\\.(js|jsx|ts|tsx|mjs)$': [
+      '@swc/jest',
+      {
+        jsc: {
+          parser: {
+            syntax: 'typescript',
+            tsx: true,
+          },
+          transform: {
+            react: {
+              runtime: 'automatic',
+            },
+          },
+        },
+      },
+    ],
+  },
+  transformIgnorePatterns: [
+    '/node_modules/(?!(msw|@mswjs|@open-draft|until-async|undici|rettime)/)',
+  ],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
 };

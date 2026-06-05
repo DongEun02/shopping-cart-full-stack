471b30acd73e05c5e1480c8ac4db8936b906bcc2
eunnn <ehddms0517@naver.com>
2026-06-05 16:05:17 +0900

    chore: 이미지 경로 수정

--- server/src/database/inMemoryDatabase.ts
+++ server/src/database/inMemoryDatabase.ts
@@ -5,16 +5,32 @@ import type { ProductId } from '../types/type.ts';
 export const products = new Map<ProductId, Product>();
 export const shoppingCart = new ShoppingCart();
 
+const getServerBaseUrl = () => {
+  if (process.env.SERVER_BASE_URL) {
+    return process.env.SERVER_BASE_URL;
+  }
+
+  if (process.env.RAILWAY_PUBLIC_DOMAIN) {
+    return `https://${process.env.RAILWAY_PUBLIC_DOMAIN}`;
+  }
+
+  return 'http://localhost:3000';
+};
+
+const createImageUrl = (fileName: string) => {
+  return `${getServerBaseUrl()}/images/${fileName}`;
+};
+
 const seedProducts = [
   new Product({
     name: '상품이름A',
     price: 35000,
-    image: 'http://localhost:3000/images/productA.svg',
+    image: createImageUrl('productA.svg'),
   }),
   new Product({
     name: '상품이름B',
     price: 25000,
-    image: 'http://localhost:3000/images/productB.svg',
+    image: createImageUrl('productB.svg'),
   }),
 ];
 
